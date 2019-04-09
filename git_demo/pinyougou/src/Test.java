f38d1e032bbd8253eef183d9274cfdd15eee4533
zjw <942961221@qq.com>
2019-04-09 20:42:28 +0800

    品优购项目二

---
+++ git_demo/pinyougou/src/Test.java
@@ -0,0 +1,14 @@
+/**
+ * @author : zhangJW
+ * @Description : java类作用描述
+ * @CreateDate : 2019/4/9 20:33
+ * @Updat :  更新作者
+ * @UpdateDate : 2019/4/9 20:33
+ * @UpdateRemark : 更新内容注释
+ * @Version : 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(1111);
+    }
+}
